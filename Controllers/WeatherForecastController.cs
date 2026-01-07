11915966a36463a31c7abcc583836aa1351662a2
Akshay Kumar <akshay.k@helpxpress.com>
2026-01-07 15:23:23 +0530

    ter

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -30,16 +30,5 @@ namespace JenkinsAPI.Controllers
             .ToArray();
         }
 
-        [HttpGet(Name = "GetWeathet")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
